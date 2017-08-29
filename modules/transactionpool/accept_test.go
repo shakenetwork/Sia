86eea5a6be661c7123ced813647e25e58022c989
Ava Howell <ava@avahowell.me>
2017-08-28 20:32:32 -0400

    update modules with wallet context

--- modules/transactionpool/accept_test.go
+++ modules/transactionpool/accept_test.go
@@ -24,7 +24,7 @@ func TestAcceptTransactionSet(t *testing.T) {
 	}
 
 	// Create a valid transaction set using the wallet.
-	txns, err := tpt.wallet.SendSiacoins(types.NewCurrency64(100), types.UnlockHash{})
+	txns, err := tpt.wallet.SendSiacoins(types.NewCurrency64(100), types.UnlockHash{}, modules.DefaultWalletContext)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -179,7 +179,7 @@ func TestCheckMinerFees(t *testing.T) {
 	}
 
 	// Add a transaction that has sufficient fees.
-	_, err = tpt.wallet.SendSiacoins(types.SiacoinPrecision.Mul64(50), types.UnlockHash{})
+	_, err = tpt.wallet.SendSiacoins(types.SiacoinPrecision.Mul64(50), types.UnlockHash{}, modules.DefaultWalletContext)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -242,7 +242,7 @@ func TestTransactionGraph(t *testing.T) {
 
 	// Create a transaction sending money to an output that TransactionGraph can
 	// spent (the empty UnlockConditions).
-	txns, err := tpt.wallet.SendSiacoins(types.SiacoinPrecision.Mul64(100), types.UnlockConditions{}.UnlockHash())
+	txns, err := tpt.wallet.SendSiacoins(types.SiacoinPrecision.Mul64(100), types.UnlockConditions{}.UnlockHash(), modules.DefaultWalletContext)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -284,7 +284,7 @@ func TestTransactionGraphDiamond(t *testing.T) {
 
 	// Create a transaction sending money to an output that TransactionGraph can
 	// spent (the empty UnlockConditions).
-	txns, err := tpt.wallet.SendSiacoins(types.SiacoinPrecision.Mul64(100), types.UnlockConditions{}.UnlockHash())
+	txns, err := tpt.wallet.SendSiacoins(types.SiacoinPrecision.Mul64(100), types.UnlockConditions{}.UnlockHash(), modules.DefaultWalletContext)
 	if err != nil {
 		t.Fatal(err)
 	}
